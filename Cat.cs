d8cb3ee46686b4c855215b0c476bbfcf443f3492
Dagramon <karamov-damir228@mail.ru>
2026-06-06 10:00:36 +0500

    first

---
+++ Cat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd1_Romanov
+{
+    class Cat
+    {
+        private string name;
+        private double weight;
+
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.WriteLine("Вес должен быть положительным");
+                }
+            }
+        }
+
+        public string Info()
+        {
+            return $"Имя кота: {name}\n" +
+                $"Вес кота: {weight}";   
+        }
+
+        
+
+        public Cat(string name, double weight) // конструктор с установкой имени
+        {
+            this.SetCatName(name);
+            Weight = weight;
+        }
+
+        public string Name // свойство имени
+        {
+            get { return name; }
+            set { SetCatName(value); }
+        }
+
+        public void Meow() // голос кота
+        {
+            Console.WriteLine($"{name}:Маяууу!!!!") ;
+        }
+
+        private void SetCatName(string Name) // установка имени кота
+        {
+            bool onlyLetters = true;
+
+            foreach (var item in Name)
+            {
+                if (!char.IsLetter(item))
+                {
+                    onlyLetters = false;
+                }
+
+
+            }
+
+            if (onlyLetters)
+            {
+                name = Name;
+            }
+            else
+            {
+                Console.WriteLine($"{Name} - неправильное имя");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+namespace zd1_Romanov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<Cat> cats = new List<Cat>();
+                
+                int amount = 0;
+                do
+                {
+                    Console.WriteLine("Введите кол-во котов:");
+                    amount = int.Parse(Console.ReadLine());
+                } 
+                while (amount <= 0);
+
+
+
+                for (int i = 0; i < amount; i++)
+                {
+                    Console.WriteLine("Введите имя кота");
+                    string CatName = Console.ReadLine();
+                    
+                    double weight = 0;
+                    do
+                    {
+                        Console.WriteLine($"Введите вес кота {CatName}");
+                        weight = double.Parse(Console.ReadLine());
+                    }
+                    while (amount <= 0);
+
+                    Cat cat = new Cat(CatName,weight);
+                    cats.Add(cat);
+                }
+
+                foreach (var item in cats)
+                {
+                    Console.WriteLine(item.Info());
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Нажмите любую кнопку, чтобы продолжить");
+                Console.ReadKey();
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Некорректный тип данных");
+                Console.ReadKey();
+
+            }
+            
+        }
+    }
+}
